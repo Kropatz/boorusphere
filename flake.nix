662b0b6862c6fcfe32dc529400c526a0bd574eaa
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-06-27 19:28:17 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Boorusphere";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
+        buildToolsVersion = "30.0.3";
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          buildToolsVersions = [ buildToolsVersion ];
+          platformVersions = [ "31" "30" "34" "33" "32" "29" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+        };
+        androidSdk = androidComposition.androidsdk;
+      in {
+        devShell = with pkgs;
+          mkShell rec {
+            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+            buildInputs = [ flutter androidSdk jdk17 dart ];
+          };
+      });
+}
